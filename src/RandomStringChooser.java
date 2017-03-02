141ac78189e84a7a10826aa71efe6744264569e3
David Dunn <DaveD@Leagues-iMac.attlocal.net>
2017-03-02 13:11:07 -0800

    init

---
+++ src/RandomStringChooser.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class RandomStringChooser
+{
+	private ArrayList<String> choices;
+	
+    //add code here for Part A of Question 1
+     
+    public String toString()
+    {
+    	return "" + choices;
+    }    
+}
